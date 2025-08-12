225bde1c13e7b5c4a359332d867540220787ab07
williabrac <willianbr.ac68@gmail.com>
2025-08-11 22:32:54 -0300

    feat: Add button component and global styles, update dependencies
    - Introduced a new `Button` component with customizable variants and sizes.
    - Added global styles in `globals.css` for theming and layout consistency.
    - Created a new `components.json` configuration file for component aliases and settings.
    - Updated dependencies in `package.json` and `package-lock.json`, including `@radix-ui/react-slot`, `lucide-react`, `postcss`, and `tw-animate-css`.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button variant="destructive">Button</Button>
+    </div>
+  );
 }
